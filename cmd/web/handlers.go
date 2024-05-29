18553e3969759b1ff8978a0edcaff15a84d3256b
chris <chris@joycenet.us>
2024-05-29 16:00:29 -0500

    Chapter 25

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,10 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
 )
 
 func (app *application) Home(writer http.ResponseWriter, reader *http.Request) {
-	fmt.Fprint(writer, "This is the home page.")
+	//fmt.Fprint(writer, "This is the home page.")
+	_ = app.render(writer, reader, "home.page.gohtml", &TemplateData{})
+}
+
+type TemplateData struct {
+	IP   string
+	Data map[string]any
+}
+
+func (app *application) render(writer http.ResponseWriter, reader *http.Request, templt string, data *TemplateData) error {
+	parsedTemplate, err := template.ParseFiles("./templates/" + templt)
+	if err != nil {
+		http.Error(writer, "bad request", http.StatusBadRequest)
+		return err
+	}
+
+	err = parsedTemplate.Execute(writer, data)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
